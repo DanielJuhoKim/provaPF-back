3661c7f12d977800891939ab6e018719fab7bcd3
DanielJuhoKim <danieljk@al.insper.edu.br>
2025-11-10 09:50:43 -0300

    base

---
+++ src/main/java/com/example/provaPF/ProvaPfApplication.java
@@ -0,0 +1,13 @@
+package com.example.provaPF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaPfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaPfApplication.class, args);
+	}
+
+}
